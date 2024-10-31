398b20c9c063f051ec1a3b53e00116f5de50e9a9
sasha-kantoriz <no.money.no.honey5151@gmail.com>
2024-10-31 21:42:25 +0200

    Add processing of books with editors

--- guttenberg2.py
+++ guttenberg2.py
@@ -452,7 +452,7 @@ def get_books(run_folder, start, end, interior_only=False, cover_only=False, wor
             #
             book_txt = response.content.decode('utf-8')
             #
-            book_author = re.search(r"Author: (.*)\n", book_txt)
+            book_author = re.search(r"(Author|Editor): (.*)\n", book_txt, re.IGNORECASE)
             book_author = book_author.groups()[0] if book_author else ""
             book_author = book_author.strip().replace('\\', '-').replace('/', '-').replace('&', ' and ')
             book_language = re.search(r"Language: (.*)\n", book_txt, re.IGNORECASE)
